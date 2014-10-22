2f49b7f73374f5edb62b633d02164db50ecdc722
Admin <Admin@SERG>
2014-10-22 13:00:38 +0400

    Добавлена инфраструктура тестирования

---
+++ test/ptk/AllTests.java
@@ -0,0 +1,13 @@
+package ptk;
+
+import ptk.ConverterTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    ConverterTest.class,
+})
+public class AllTests {
+
+}
