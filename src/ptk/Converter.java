b472c6281fcfe13bbb243a370ae4b32ba793fc22
Sergey <>
2014-11-19 17:08:52 +0400

    Метод constructGerberString() выделен из convert()

--- src/ptk/Converter.java
+++ src/ptk/Converter.java
@@ -59,23 +59,7 @@ public class Converter {
             w.write(gerber.getHeader());
             while (r.ready()) {
                 linesCount++;
-                String line = r.readLine();
-                if (lineHasErrors(line)) {
-                    System.err.println("ERROR: Skipping error line " + linesCount + ": \"" + line + "\"");
-                    continue;
-                }
-                String[] data = line.split(delimeter, 4);
-                String result = "";
-                result += data[2].isEmpty() ? "" : "X" + gerber.formatNumber(data[2]);
-                result += data[3].isEmpty() ? "" : "Y" + gerber.formatNumber(data[3]);
-                if (!data[0].isEmpty()) {
-                    polygonsCount++;
-                    result = "G36*\n" + result + "D02*\n";
-                    if (polygonsCount == 2 && gerber.isLayersFile()) result = "%LPC*%\n" + result;
-                    if (polygonsCount > 1) result = "G37*\n" + result;
-                } else
-                    result += "D01*\n";
-                w.write(result);
+                w.write(constructGerberString(r.readLine()));
             }
             w.write("G37*\n");
             w.write("M02*\n");
@@ -88,4 +72,22 @@ public class Converter {
         }
     }
     
+    private String constructGerberString(String line) {
+        if (lineHasErrors(line)) {
+            System.err.println("ERROR: Skipping error line " + linesCount + ": \"" + line + "\"");
+            return "";
+        }
+        String[] data = line.split(delimeter, 4);
+        String result = "";
+        result += data[2].isEmpty() ? "" : "X" + gerber.formatNumber(data[2]);
+        result += data[3].isEmpty() ? "" : "Y" + gerber.formatNumber(data[3]);
+        if (!data[0].isEmpty()) {
+            polygonsCount++;
+            result = "G36*\n" + result + "D02*\n";
+            if (polygonsCount == 2 && gerber.isLayersFile()) result = "%LPC*%\n" + result;
+            if (polygonsCount > 1) result = "G37*\n" + result;
+        } else
+            result += "D01*\n";
+        return result;
+    }
 }
