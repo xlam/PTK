559f8e6565a60863075f706e67b6261944cc0a66
Sergey <>
2014-11-21 17:08:30 +0400

    Version 0.4.4-dev started.

--- src/ptk/PTK.java
+++ src/ptk/PTK.java
@@ -7,7 +7,7 @@ package ptk;
 
 public class PTK {
     
-    public static final String VERSION = "0.4.3";
+    public static final String VERSION = "0.4.4-dev";
     
     public static void main(String[] args) {
         System.out.println("PTK v" + VERSION + ".\n");
