ae1fb914801d52ffe4b7ef55ae7760941b138535
Sergey <>
2014-11-21 16:33:46 +0400

    Removed useless license template from beginning of files. Fixed @author to Sergey (me :))

--- test/ptk/ConverterTest.java
+++ test/ptk/ConverterTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ptk;
 
 import java.io.BufferedReader;
@@ -16,7 +10,7 @@ import org.junit.Test;
 
 /**
  *
- * @author Admin
+ * @author Sergey
  */
 public class ConverterTest {
     
